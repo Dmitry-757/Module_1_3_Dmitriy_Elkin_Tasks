21ad40e6842178c13cc65e148888cc99327947eb
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2022-11-25 21:19:11 +0300

    add multiple catches example

---
+++ src/main/java/com/Dmitry_Elkin/chapter10/MultipleCatches.java
@@ -0,0 +1,25 @@
+package com.Dmitry_Elkin.chapter10;
+
+import java.util.Random;
+
+public class MultipleCatches {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = 0, b;
+
+        int[] arr = new int[100];
+        for (int i = 0; i < 32000; i++) {
+            try {
+                b = r.nextInt(0, 110);
+                a = 123 / b;
+
+                arr[b] = a;
+            } catch (ArithmeticException e) {
+                System.out.println("arithmetical exception " + e);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("ArrayIndexOutOfBoundsException " + e);
+            }
+            System.out.println("a = "+a);
+        }
+    }
+}
